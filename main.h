afdf03a4c543d82630d12913f8814b3cc81fd4d7
mulex12 <mulex.john@gmail.com>
2022-07-26 07:04:40 -0700

    m

--- main.h
+++ main.h
@@ -15,6 +15,7 @@ int print_octal(va_list o);
 int print_x(va_list x);
 int print_X(va_list X);
 int print_rot13(va_list R);
+int (*find_function(const char *format))(va_list);
 /**
   * struct code_format - Struct format
   *
