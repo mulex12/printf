5a6fa5cabdd93b90f1164587f5abd61689e50eb0
mulex12 <mulex.john@gmail.com>
2022-08-03 10:22:11 -0700

    hj

--- _printf.c
+++ _printf.c
@@ -1,78 +1,75 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 /**
-  * find_function - function that finds formats for _printf
-  * calls the corresponding function.
-  * @format: format (char, string, int, decimal)
-  * Return: NULL or function associated ;
-  */
-int (*find_function(const char *format))(va_list)
+ * opFunction - Second function for _printf
+ * @cont: Contador
+ * @list: Lista arguments
+ * @tipos: Tipos que nos trae el JSON
+ * @format: Formato
+ * Return: Len
+*/
+int opFunction(int cont, va_list list, typedate tipos[], const char *format)
 {
-	unsigned int i = 0;
-	code_f find_f[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
-		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot13},
-		{NULL, NULL}
-	};
-
-	while (find_f[i].sc)
-	{
-		if (find_f[i].sc[0] == (*format))
-			return (find_f[i].f);
-		i++;
-	}
-	return (NULL);
-}
-/**
-  * _printf - function that produces output according to a format.
-  * @format: format (char, string, int, decimal)
-  * Return: size the output text;
-  */
-int _printf(const char *format, ...)
+int i = 0, j = 0, band = 0, spaces = 0;
+while (format && format[i])
 {
-	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, cprint = 0;
-
-	if (format == NULL)
-		return (-1);
-	va_start(ap, format);
-	while (format[i])
+	band = 0;
+	if (format[i] == '%' && format[i + 1] == '\0')
+	return (-1);
+	else if (format[i] == '%' && format[i + 1] != '%')
 	{
-		while (format[i] != '%' && format[i])
+		for (j = 0; j < 7; j++)
+		{
+			if (format[i] == '%' && format[i + spaces + 1] == tipos[j].typec)
+			{
+				cont += tipos[j].fun(list);
+				i += spaces + 1;
+				band = 1;
+			}
+		}
+		if (band == 0 && format[i + 2] != '\n')
 		{
 			_putchar(format[i]);
-			cprint++;
-			i++;
+			cont++;
+
 		}
-		if (format[i] == '\0')
-			return (cprint);
-		f = find_function(&format[i + 1]);
-		if (f != NULL)
+		else if (band == 0 && format[i + 2] == '\n')
 		{
-			cprint += f(ap);
-			i += 2;
-			continue;
+			_putchar(format[i]);
+			cont++;
 		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		cprint++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
 	}
-	va_end(ap);
-	return (cprint);
+	else if (format[i] == '%' && format[i + 1] == '%')
+	{
+		i += _putchar('%');
+		cont += 1;
+	}
+	else
+		cont += _putchar(format[i]);
+	i++;
+}
+	return (cont);
+}
+/**
+ * _printf - Printf!!
+ * @format: Format
+ * Return: len
+ */
+int _printf(const char *format, ...)
+{
+typedate tipos[] = {
+	{'s', printString}, {'c', printChar}, {'i', printInteger},
+	{'d', printDecimal}, {'b', printBinary}, {'o', printOctal},
+	{'u', printUnsigned}
+};
+va_list list;
+int cont = -1;
+
+if (format != NULL)
+{
+va_start(list, format);
+cont = 0;
+cont = opFunction(cont, list, tipos, format);
+va_end(list);
+}
+return (cont);
 }
